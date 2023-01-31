215080744885b1f5ce334b984589dc5272e23fcf
“Francesca <dascanio.francesca@gmail.com>
2023-01-31 15:17:25 +0100

    Completato esercizio

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,8 @@ const { createApp } = Vue;
 createApp ({
     data () {
         return {
-            
+            // lui deve diventare un array di 10 email 
+            emails: [],
         }
     },
     methods: {
@@ -11,6 +12,21 @@ createApp ({
     },
     mounted () {
 
+        for (let i = 1; i <= 10; i++) {
+
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((resp) => {
+                
+                const email = resp.data.response;
+
+                this.emails.push(email);
+
+            })
+
+        }
+
+        console.log(this.emails);
     }
 }
 ).mount('#app')
